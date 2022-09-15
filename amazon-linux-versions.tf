b473c739831cf36754ad3d1427a641fe9a8a5f14
gwendo567 <gwendolynmprng@gmail.com>
2022-09-15 15:18:57 +0200

    Add files via upload

---
+++ amazon-linux-versions.tf
@@ -0,0 +1,21 @@
+
+
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+data "aws_ami" "amazon-linux-2-kernel-5" {
+  most_recent = true
+  owners      = ["amazon"]
+  
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5*"]
+  }
+}
---
+++ app-variables.tf
@@ -0,0 +1,15 @@
+
+## Application Module - Variables ##
+
+
+# Application definition
+
+variable "app_name" {
+  type        = string
+  description = "TakehomeTest"
+}
+
+variable "app_environment" {
+  type        = string
+  description = "TakehomeTest"
+}
